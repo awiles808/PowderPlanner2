dcb1948a3d562ef62dc017b7568ca0a3858b1aff
awiles808 <awiles808@gmail.com>
2018-05-27 16:33:19 -0600

    Grabbed Dependencies set route started server all in server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 //grab dependencies
+const express = require('express'),
+  app = express();
+    port = process.env.PORT || 8080
 //config application
+
 //set the routes
+app.get('/', (req, res) => {
+  res.send('Hello, I is the app!')
+});
+
 //start server
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
